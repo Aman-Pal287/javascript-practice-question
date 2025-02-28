0996bbacc26c17e911154ea5a12f3b1b23a48f7b
Aman-Pal287 <palaman9786@gmail.com>
2025-02-28 19:56:33 +0530

    day 22 assignment is completed

---
+++ day22/app.js
@@ -0,0 +1,54 @@
+//1. write a for loop to print number from 10 to 1 
+// for(i=10; i>=1;i--){
+//     console.log(i)
+// }
+// let a = 10;
+// while(a>=1){
+//     console.log(a)
+//     a--;
+// }
+
+// 2. use a while loop to print multiples of 3 from 3 to 30
+// let num = 3;
+// while(num <= 30){
+//     console.log(num);
+//     num += 3;
+// }
+
+// 3. write a program to calculate the sum of numbers from 1 to 100 using a loop
+
+// let sum = 0;
+
+//for loop
+// for (i=1; i<=100;i++){
+//     sum+= i;
+// }
+//while loop
+// let i=1;
+// while(i<=100){
+//     sum+=i
+//     i++
+// }
+
+// console.log(sum)
+
+
+// 4. create a nested loop to print a star pattern
+// for (let i = 1; i<=5; i++){
+//     let stars = "";
+//     for (let j = 1;j<=i;j++){
+//         stars += "*"
+//     }
+//     console.log(stars);
+// }
+
+
+
+
+
+// 5.Use a for loop to iterate over the string "javaScript"
+// let str = "javaScript";
+
+// for (let i of str){
+//     console.log(i)
+// }
