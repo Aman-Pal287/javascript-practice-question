f4218a190fd3e494896be99ca9346dc0a9fd06be
Aman-Pal287 <palaman9786@gmail.com>
2025-03-04 15:12:21 +0530

    Q1 is done

---
+++ day24(closures)/app.js
@@ -0,0 +1,7 @@
+// Q1 .Create a function that takes another function as an argument and calls it after 3 seconds (HOF + Callback).
+// function callerfnc(fn){
+//     setTimeout(fn,3000);
+// }
+// callerfnc(function(){
+//     console.log("makra")
+// })
