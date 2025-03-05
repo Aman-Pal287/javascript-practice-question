76922d64cbfdba0019d5da6d9ca47a45ccc6c6ee
Aman-Pal287 <palaman9786@gmail.com>
2025-03-05 13:42:39 +0530

    Q1 is done

---
+++ day25/app.js
@@ -0,0 +1,9 @@
+//Q1. Create a function that takes a callback and executes it after every `n` seconds indefinitely.
+
+// function barbarchalao(fn,time){
+//     setInterval(fn,time);
+// }
+
+// barbarchalao(function(){
+//     console.log("hey");
+// },2000)
