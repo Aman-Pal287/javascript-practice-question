084170b35da790636a64c8055b1e67ddd48c6e6b
Aman-Pal287 <palaman9786@gmail.com>
2025-03-05 13:53:42 +0530

    Q2 is done

--- day25/app.js
+++ day25/app.js
@@ -7,3 +7,36 @@
 // barbarchalao(function(){
 //     console.log("hey");
 // },2000)
+
+
+//Q2. Implement a function that returns a function with a preset greeting (Closure).
+
+//mera dimag ka solution
+// function abcd (){
+//     let greet = "Hello"
+//     return function(name){
+//         console.log(`${greet} ${name}`)
+//     }
+// }
+
+// let greeting = abcd();
+// greeting("aman");
+// greeting("harsh");
+// greeting("shubham");
+// greeting("govinda");
+// greeting("roushan");
+// greeting("dkpl");
+
+
+// sir ka dimag ka solution
+
+// function greetkaro(greeting){
+//     return function(name){
+//         console.log(`${greeting}! ${name}`)
+//     }
+// }
+// let greetingFunction  = greetkaro("Hello");
+// greetingFunction("aman");
+// greetingFunction("aman");
+// greetingFunction("shubham");
+// greetingFunction("dkpl");
